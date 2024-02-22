b9622fb7c7d9c0713e7bc89290353c2b0467847d
taitd-gms <142890841+trinhdinhtai@users.noreply.github.com>
2024-02-22 09:58:01 +0700

    feat: add Toaster component to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import { Toaster } from "@/components/ui/sonner"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -21,8 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Toaster />
         <TailwindIndicator />
+        {children}
       </body>
     </html>
   )
